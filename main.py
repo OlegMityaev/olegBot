367ace012a77062258e4e1dfde2dc85e81a8720f
mitya <mityaev2222@gmail.com>
2022-04-16 01:49:09 +0300

    говорит но женским голосом команда porofly

--- main.py
+++ main.py
@@ -6,8 +6,10 @@ import speech_recognition as sr
 import os
 import pyttsx3
 import time
+import random
 from pydub import AudioSegment
 
+print(os.getcwd() + "\\ffmpeg\\bin\\ffmpeg.exe")
 AudioSegment.converter = os.getcwd() + "\\ffmpeg\\bin\\ffmpeg.exe"
 AudioSegment.ffmpeg = os.getcwd() + "\\ffmpeg\\bin\\ffmpeg.exe"
 AudioSegment.ffprobe = os.getcwd() + "\\ffmpeg\\bin\\ffprobe.exe"
@@ -58,24 +60,10 @@ token = '5114669449:AAFBSdAX9N0dhcs71r6U4oICP1_AdBriWVo'
 bot = telebot.TeleBot(token)
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id,'Начало работы бота')
+    bot.send_message(message.chat.id, 'Начало работы бота')
     bot.send_message(message.chat.id, 'Введите /com для просмотра списка комманд')
-@bot.message_handler(commands=['PoRofly'])
-def rofl(message):
-    bot.send_message(message.chat.id, 'Обкодился')
-    text = message.text.lower()
-    if '/PoRofly' in text and text.split(' ')[0] == 'PoRofly':
-        src = str('Напитонил') + '_answer.oga'
-        text_to_speach.save_to_file(text[9:], src)
-        text_to_speach.runAndWait()
-        time.sleep(1)
-        voice = open(src, 'rb')
-        bot.send_audio(message.chat.id, voice)
-    bot.send_photo(message.chat.id, 'http://img2.safereactor.cc/pics/post/full/it-%D1%8E%D0%BC%D0%BE%D1%80-geek-doge-%D0%9C%D0%B5%D0%BC%D1%8B-5952251.png')
-    bot.send_message(message.chat.id, 'Обпитонился')
-    bot.send_photo(message.chat.id, 'https://cs12.pikabu.ru/post_img/2021/05/19/1/1621376414120131742.png')
-    bot.send_message(message.chat.id, 'Обсишарпился')
-    bot.send_photo(message.chat.id, 'https://avatars.mds.yandex.net/i?id=3408794366b2cb0e99df8719c128b8cd-5173372-images-thumbs&n=13')
+
+
 @bot.message_handler(commands=['info'])
 def info(message):
     bot.send_message(message.chat.id, 'Здравствуйте! Меня зовут Олег и я Ваш личный гид по Кванториуму!')
@@ -92,56 +80,8 @@ def d(message):
     bot.send_message(message.chat.id,'/start - начало')
     bot.send_message(message.chat.id,'/info - инфо')
     bot.send_message(message.chat.id, '/Raspisanie - расписание квантумов')
-@bot.message_handler(content_types=["voice"])
-def bot_messages(message):
-    file_info = bot.get_file(message.voice.file_id)
-    file = requests.get('https://api.telegram.org/file/bot{0}/{1}'.format(token, file_info.file_path))
-    src = file_info.file_path[:6] + 'oga' + file_info.file_path[5:]
-    dst = file_info.file_path[:6] + 'wav' + file_info.file_path[5:-3] + 'wav'
-    with open(src,'wb') as f:
-        f.write(file.content)
-    sound = AudioSegment.from_oga(src)
-    sound.export(dst, format="wav")
-    del sound
-    rec = sr.Recognizer()
-    with sr.WavFile(dst) as source:
-        audio = rec.record(source)
-    try:
-        text = rec.recognize_google(audio, language="ru-RU").lower()
-        error = 0
-    except LookupError:
-        bot.send_message(message.chat.id, 'Не понимаю Ваш восхитительный голос :(')
-        error = 1
-    if error == 0:
-        if 'произнеси' in text and text.split(' ')[0] == 'произнеси':
-            text_to_speach.save_to_file(text[9:], file_info.file_path[:6] + 'answer' + file_info.file_path[5:])
-            text_to_speach.runAndWait()
-            time.sleep(1)
-            voice = open( file_info.file_path[:6]+'answer'+ file_info.file_path[5:], 'rb')
-            bot.send_audio(message.chat.id, voice)
-        elif 'повтори' in text and text.split(' ')[0] == 'повтори':
-            bot.send_message(message.chat.id, 'Повторяю: ' + text[7:])
-        elif 'сайт' == text:
-            bot.send_message(message.chat.id, 'Сайт NTA: https://newtechaudit.ru/')
-        elif 'своё имя' in text or 'как тебя зовут' in text or 'назови себя' in text:
-            bot.send_message(message.chat.id,'Меня зовут Bot!')
-        elif 'случайное число' in text and 'от' in text and 'до' in text:
-            ot=text.find('от')
-            do=text.find('до')
-            f_num=int(text[ot+3:do-1])
-            l_num=int(text[do+3:])
-            bot.send_message(message.chat.id, str(random.randint(f_num, l_num)))
-        elif 'random ' in text:
-            if len(text.split(' ')) == 2:
-                bot.send_message(message.chat.id, str(random.randint(0, int(text.split(' ')[1]))))
-            elif len(text.split(' ')) == 3:
-                bot.send_message(message.chat.id, str(random.randint(int(text.split(' ')[1]), int(text.split(' ')[2]))))
-            else:
-                bot.send_message(message.chat.id, 'Неверный формат. Попробуйте: "rand X" или "rand X Y"')
-        elif 'пока' == text or 'до свидания' == text:
-            bot.send_message(message.chat.id, 'До свидания!')
-    else:
-        bot.send_message(message.chat.id, 'Не понял команду :(')
+
+
 @bot.message_handler(commands=['Raspisanie'])
 def b(message):
     markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
@@ -153,6 +93,7 @@ def b(message):
     item8 = types.KeyboardButton('Хайтек')
     markup.add(item3,item4,item5,item6,item7,item8)
     bot.send_message(message.chat.id, 'Выберите интересующий квантум', reply_markup=markup)
+
 @bot.message_handler(content_types='text')
 def aaa(message):
     if message.text=='Vr/Ar':
@@ -196,5 +137,93 @@ def aaa(message):
         bot.send_message(message.chat.id, 'https://vk.com/kvantoriumpskov')
         bot.send_message(message.chat.id, 'https://vk.com/golikovaao')
         bot.send_message(message.chat.id, 'kvantoriumpskov@gmail.com')
+
+# def rofl(message):
+    text = message.text.lower()
+    print(message.message_id)
+    if 'porofly' in text: #and text.split(' ')[0] == 'porofly':
+        mid = bot.send_message(message.chat.id, 'Обкодился')
+        btext = mid.text.lower()
+        src = str(message.chat.id) + str(mid.message_id-1) + '_answer.oga'
+        print(src)
+        text_to_speach.save_to_file(btext[:], src)
+        text_to_speach.runAndWait()
+        time.sleep(1)
+        voice = open(src, 'rb')
+        bot.send_audio(message.chat.id, voice)
+        bot.send_photo(message.chat.id, 'http://img2.safereactor.cc/pics/post/full/it-%D1%8E%D0%BC%D0%BE%D1%80-geek-doge-%D0%9C%D0%B5%D0%BC%D1%8B-5952251.png')
+
+        mid = bot.send_message(message.chat.id, 'Обпитонился')
+        btext = mid.text.lower()
+        src = str(message.chat.id) + str(mid.message_id - 1) + '_answer.oga'
+        print(src)
+        text_to_speach.save_to_file(btext[:], src)
+        text_to_speach.runAndWait()
+        time.sleep(1)
+        voice = open(src, 'rb')
+        bot.send_audio(message.chat.id, voice)
+        bot.send_photo(message.chat.id, 'https://cs12.pikabu.ru/post_img/2021/05/19/1/1621376414120131742.png')
+
+        mid = bot.send_message(message.chat.id, 'Обсишарпился')
+        btext = mid.text.lower()
+        src = str(message.chat.id) + str(mid.message_id - 1) + '_answer.oga'
+        print(src)
+        text_to_speach.save_to_file(btext[:], src)
+        text_to_speach.runAndWait()
+        time.sleep(1)
+        voice = open(src, 'rb')
+        bot.send_audio(message.chat.id, voice)
+        bot.send_photo(message.chat.id, 'https://avatars.mds.yandex.net/i?id=3408794366b2cb0e99df8719c128b8cd-5173372-images-thumbs&n=13')
+
+@bot.message_handler(content_types=["voice"])
+def bot_messages(message):
+    file_info = bot.get_file(message.voice.file_id)
+    file = requests.get('https://api.telegram.org/file/bot{0}/{1}'.format(token, file_info.file_path))
+    src = file_info.file_path[:6] + 'oga' + file_info.file_path[5:]
+    dst = file_info.file_path[:6] + 'wav' + file_info.file_path[5:-3] + 'wav'
+    with open(src, 'wb') as f:
+        f.write(file.content)
+    sound = AudioSegment.from_oga(src)
+    sound.export(dst, format="wav")
+    del sound
+    rec = sr.Recognizer()
+    with sr.WavFile(dst) as source:
+        audio = rec.record(source)
+    try:
+        text = rec.recognize_google(audio, language="ru-RU").lower()
+        error = 0
+    except LookupError:
+        bot.send_message(message.chat.id, 'Не понимаю Ваш восхитительный голос :(')
+        error = 1
+    if error == 0:
+        if '' in text: # and text.split(' ')[0] == 'porofly':
+            text_to_speach.save_to_file(text[:], file_info.file_path[:6] + 'answer' + file_info.file_path[5:])
+            text_to_speach.runAndWait()
+            time.sleep(1)
+            voice = open(file_info.file_path[:6]+'answer'+ file_info.file_path[5:], 'rb')
+            bot.send_audio(message.chat.id, voice)
+        elif 'повтори' in text and text.split(' ')[0] == 'повтори':
+            bot.send_message(message.chat.id, 'Повторяю: ' + text[7:])
+        elif 'сайт' == text:
+            bot.send_message(message.chat.id, 'Сайт NTA: https://newtechaudit.ru/')
+        elif 'своё имя' in text or 'как тебя зовут' in text or 'назови себя' in text:
+            bot.send_message(message.chat.id,'Меня зовут Bot!')
+        elif 'случайное число' in text and 'от' in text and 'до' in text:
+            ot=text.find('от')
+            do=text.find('до')
+            f_num=int(text[ot+3:do-1])
+            l_num=int(text[do+3:])
+            bot.send_message(message.chat.id, str(random.randint(f_num, l_num)))
+        elif 'random ' in text:
+            if len(text.split(' ')) == 2:
+                bot.send_message(message.chat.id, str(random.randint(0, int(text.split(' ')[1]))))
+            elif len(text.split(' ')) == 3:
+                bot.send_message(message.chat.id, str(random.randint(int(text.split(' ')[1]), int(text.split(' ')[2]))))
+            else:
+                bot.send_message(message.chat.id, 'Неверный формат. Попробуйте: "rand X" или "rand X Y"')
+        elif 'пока' == text or 'до свидания' == text:
+            bot.send_message(message.chat.id, 'До свидания!')
+    else:
+        bot.send_message(message.chat.id, 'Не понял команду :(')
 
 bot.infinity_polling()()
